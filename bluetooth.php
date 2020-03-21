af7fa51dd7eaae62d246b26f66897588dedad80a
Markku Leino <46004259+markkuleino@users.noreply.github.com>
2020-03-21 17:17:10 +0200

    Päivitys

--- bluetooth.php
+++ bluetooth.php
@@ -239,81 +239,6 @@ loop.run_until_complete(run(address, loop))
 #
 #
 
-#
-#
-#
-#
-#
-
-
-
-
-
-
-
-
-
-
-# -*- coding: utf-8 -*-
-"""
-Notifications
--------------
-Example showing how to add notifications to a characteristic and handle the responses.
-Updated on 2019-07-03 by hbldh <henrik.blidh@gmail.com>
-"""
-
-import logging
-import asyncio
-import platform
-
-from bleak import BleakClient
-from bleak import _logger as logger
-
-
-CHARACTERISTIC_UUID = (
-    "00002a37-0000-1000-8000-00805f9b34fb"
-)  # <--- Change to the characteristic you want to enable notifications from.
-
-
-#0000feee-0000-1000-8000-00805f9b34fb
-
-
-def notification_handler(sender, data):
-    """Simple notification handler which prints the data received."""
-    print("{0}: {1}".format(sender, data))
-
-async def run(address, loop, debug=False):
-    if debug:
-        import sys
-
-        # loop.set_debug(True)
-        l = logging.getLogger("asyncio")
-        l.setLevel(logging.DEBUG)
-        h = logging.StreamHandler(sys.stdout)
-        h.setLevel(logging.DEBUG)
-        l.addHandler(h)
-        logger.addHandler(h)
-
-    async with BleakClient(address, loop=loop) as client:
-        x = await client.is_connected()
-        logger.info("Connected: {0}".format(x))
-
-        await client.start_notify(CHARACTERISTIC_UUID, notification_handler)
-        await asyncio.sleep(5.0, loop=loop)
-        await client.stop_notify(CHARACTERISTIC_UUID)
-
-if __name__ == "__main__":
-    import os
-
-    os.environ["PYTHONASYNCIODEBUG"] = str(1)
-    address = (
-        "EF:B7:AA:FC:96:73"  # <--- Change to your device's address here if you are using Windows or Linux
-        if platform.system() != "Darwin"
-        else "243E23AE-4A99-406C-B317-18F1BD7B4CBE"  # <--- Change to your device's address here if you are using macOS
-    )
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(run(address, loop, True))
-
 
 
 
@@ -628,6 +553,69 @@ Descriptor
 <h4>Heart Rate </h4>
 <p>The heart rate measurement values looks the following, BUT there is a big problem as the values are not INT as depicted here. Actually the values are BINARY data that needs to be interpreted correct.</p>
 
+
+<pre><code>
+# -*- coding: utf-8 -*-
+"""
+Notifications
+-------------
+Example showing how to add notifications to a characteristic and handle the responses.
+Updated on 2019-07-03 by hbldh <henrik.blidh@gmail.com>
+"""
+
+import logging
+import asyncio
+import platform
+
+from bleak import BleakClient
+from bleak import _logger as logger
+
+
+CHARACTERISTIC_UUID = (
+    "00002a37-0000-1000-8000-00805f9b34fb"
+)  # <--- Change to the characteristic you want to enable notifications from.
+
+def notification_handler(sender, data):
+    """Simple notification handler which prints the data received."""
+    print("{0}: {1}".format(sender, data))
+
+async def run(address, loop, debug=False):
+    if debug:
+        import sys
+
+        # loop.set_debug(True)
+        l = logging.getLogger("asyncio")
+        l.setLevel(logging.DEBUG)
+        h = logging.StreamHandler(sys.stdout)
+        h.setLevel(logging.DEBUG)
+        l.addHandler(h)
+        logger.addHandler(h)
+
+    async with BleakClient(address, loop=loop) as client:
+        x = await client.is_connected()
+        logger.info("Connected: {0}".format(x))
+
+        await client.start_notify(CHARACTERISTIC_UUID, notification_handler)
+        await asyncio.sleep(5.0, loop=loop)
+        await client.stop_notify(CHARACTERISTIC_UUID)
+
+if __name__ == "__main__":
+    import os
+
+    os.environ["PYTHONASYNCIODEBUG"] = str(1)
+    address = (
+        "EF:B7:AA:FC:96:73"  # <--- Change to your device's address here if you are using Windows or Linux
+        if platform.system() != "Darwin"
+        else "243E23AE-4A99-406C-B317-18F1BD7B4CBE"  # <--- Change to your device's address here if you are using macOS
+    )
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run(address, loop, True))
+
+
+
+</code></pre>
+
+<p>The output looks like</p>
 
 
 <pre><code>
